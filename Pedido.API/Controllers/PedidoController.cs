c7ada8ce045c7abbca3d120ea662463a224f362b
Paulo Morais <Paulo.Morais@base2.com.br>
2021-01-28 10:08:32 -0300

    Adicionar arquivos de projeto.

---
+++ Pedido.API/Controllers/PedidoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pedido.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidoController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Item 1", "Item 2" };
+        }
+    }
+}
